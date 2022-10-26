b9239724bff349de786fc00668a5772a2bf01c2f
Victor Okechukwu <VicOke360@gmail.com>
2022-10-26 22:22:16 +0100

    updates made

--- scripts.js
+++ scripts.js
@@ -1,7 +1,15 @@
-var key = document.getElementsByClassName("key")
-let color = "#18c5f5"
-key.addEventListener("click", changecolor)
+// let key = document.getElementsByClassName("key")
+// let color = "#18c5f5"
+// key.addEventListener("click", colorChange)
 
-var changecolor = function () {
- var newColor = document.getElementsByClassName("key").style.background = color
-}
+
+
+// function colorChange() {
+//  document.getElementsByClassName("key")[0].style.bax
+// }
+
+// let change = colorChange
+
+// // var changecolor = function () {
+// //  newColor = document.getElementsByClassName("key").style.background = color
+// // }
